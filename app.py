53b11b7ad94180fe662f3683ecc060c4ae39d86c
Eduardo SIlva <eduardomacedosilva@outlook.com>
2023-05-15 17:49:16 -0300

    Correções nos arquivos

--- app.py
+++ app.py
@@ -1,33 +1,6 @@
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="templates")
-<<<<<<< HEAD
-app.config['DEBUG'] = True
-
-@app.route("/")
-def index():
-    return render_template('home.html')
-
-@app.route("/sobre")
-def about():
-    return render_template('about.html')
-
-@app.route("/experiencia")
-def experience():
-    return render_template('experience.html')
-
-@app.route("/formacao")
-def training():
-    return render_template('training.html')
-
-@app.route("/contato")
-def contact():
-    return render_template('contact.html')
-
-
-if __name__ == "__main__":
-    app.run()
-=======
 
 @app.route('/')
 def index():
@@ -51,4 +24,4 @@ def contato():
 
 if __name__ == '__main__':
     app.run(port=5055, debug=True)
->>>>>>> 2cc089f (Upload da lista 4 pweb)
+
